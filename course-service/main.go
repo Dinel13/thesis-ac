82512da10e582ab770a1e28126952f2bdf7c3a85
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-11-25 10:43:16 +0800

    feat: try run grpc and rest

--- course-service/main.go
+++ course-service/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "github.com/dinel13/thesis-ac/course/app"
+import (
+	"fmt"
+	"time"
+
+	"github.com/dinel13/thesis-ac/course/app"
+	"github.com/dinel13/thesis-ac/course/grpc"
+)
 
 func main() {
-	app.StartRestServer()
-	// app.StartGRPCServer()
-	// grpc.Main()
+	go app.StartRestServer()
+	go app.StartGRPCServer()
+	// Calling Sleep method
+	time.Sleep(8 * time.Second)
+
+	// Printed after sleep is over
+	fmt.Println("Sleep Over.....")
+	grpc.Main()
 }
