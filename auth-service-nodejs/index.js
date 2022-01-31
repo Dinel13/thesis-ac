03f1e505113ce3f464921dbc61cd8153fac397b3
dinel <solahuddin.hafid.tomaballo@gmail.com>
2022-01-31 19:02:50 +0800

    rename auth directory

---
+++ auth-service-nodejs/index.js
@@ -0,0 +1,5 @@
+const startGrpc  = require("./grpc/app");
+const startRest = require("./rest/app");
+
+startGrpc();
+startRest();
---
+++ auth-service-nodejs/rest/controller.js
@@ -0,0 +1,85 @@
+const jwt = require("jsonwebtoken");
+const redis = require("../database/redis");
+
+const signup = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const token = jwt.sign({ username }, "secretKey@123");
+    await redis.set(
+      username,
+      JSON.stringify({ username, password }),
+      "EX",
+      60 * 60 * 24
+    );
+    res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    let user = await redis.get(username);
+    if (!user) {
+      return res.status(400).json({
+        message: "Username tidak ditemukan",
+      });
+    }
+    user = JSON.parse(user);
+    if (password !== user.password) {
+      return res.status(400).json({
+        message: "Password salah",
+      });
+    }
+    token = jwt.sign(
+      {
+        username,
+      },
+      "secretKey@123",
+      { expiresIn: "1d" }
+    );
+    res.status(200).json({
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error,
+    });
+  }
+};
+
+const verify = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        isAuth: false,
+      });
+    }
+
+    const decoded = jwt.verify(token, "secretKey@123");
+    if (!decoded.username) {
+      return res.status(401).json({
+        isAuth: false,
+      });
+    }
+    res.status(200).json({
+      isAuth: true,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      isAuth: false,
+    });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+  verify,
+};
