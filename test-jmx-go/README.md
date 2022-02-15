00cfb3a77da46b76ca9c770ce1690dadc5d57281
dinel <solahuddin.hafid.tomaballo@gmail.com>
2022-02-16 07:03:43 +0800

    krs-v2 for missing cpu utilization test

--- test-jmx-go/README.md
+++ test-jmx-go/README.md
@@ -7,3 +7,8 @@
  test-jmx-go/main -j=jmeter.sh -m=test /home/ubuntu/thesis-ac/jmx-go /home/ubuntu/result-go    
 
 jmeter.sh -n -t jmx/jmx-auth-go/grpc/1000/auth/verify.jmx -l auth-go/grpc/1000/verif10  
+
+
+# krs v2
+hanya untuk test cpu utilization
+go run main.go -j=/home/din/Downloads/apache-jmeter-5.4.1/bin/jmeter -m=test /home/din/project/skripsi/jmx/krs /home/din/project/skripsi/result/krs-v2
--- test-jmx-go/main.go
+++ test-jmx-go/main.go
@@ -28,15 +28,16 @@ func main() {
 	fmt.Println("sourceDir: ", sourceDir)
 	fmt.Println("destDir: ", destDir)
 
+	// read files
+	files, err := ioutil.ReadDir(sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// run jmeter test or convert jtl to html
 	if *mode == "test" {
-		runJmxTest2Jtl(sourceDir, destDir)
+		runJmxTest2Jtl(files, sourceDir, destDir)
 	} else if *mode == "convert" {
-		// read files
-		files, err := ioutil.ReadDir(sourceDir)
-		if err != nil {
-			log.Fatal(err)
-		}
 		convertJtl2html(files, sourceDir, destDir)
 	} else {
 		log.Fatal("invalid mode")
@@ -66,26 +67,39 @@ func convertJtl2html(files []fs.FileInfo, sourceDir string, destDir string) {
 
 // runJMeterTest2Jtl run jmeter test and save result to jtl
 // it need files, jmx file path, jtl file path
-func runJmxTest2Jtl(file string, destDir string) {
-	fileSave := file[:len(file)-4] // remove .jmx mislanya login
-
-	// loop 10 times
-	for i := 0; i < 10; i++ {
-		fmt.Println("iterasi ke-: ", i+1)
-		cmd := exec.Command(*jmeter, "-n", "-t", file, "-l", fmt.Sprintf("%s/%s%d.jtl", destDir, fileSave, i+1))
-		// show the result
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
+// func runJmxTest2Jtl(file string, destDir string) {
+func runJmxTest2Jtl(files []fs.FileInfo, sourceDir string, destDir string) {
+	for _, file := range files {
+		log.Println("file: ", file.Name())
+		if file.IsDir() {
+			newFiles, err := ioutil.ReadDir(fmt.Sprintf("%s/%s", sourceDir, file.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			runJmxTest2Jtl(newFiles, fmt.Sprintf("%s/%s", sourceDir, file.Name()), fmt.Sprintf("%s/%s", destDir, file.Name()))
+			continue
 		}
 
-		fmt.Println("iterasi ke-: ", i+1, " selesai")
-		fmt.Println("sleep for 3 seconds")
-		time.Sleep(time.Second * 2)
-		fmt.Println("-------------------------------------------------------")
-		fmt.Println("")
-	}
+		fileJmx := fmt.Sprintf("%s/%s", sourceDir, file.Name())
+		fileReport := file.Name()[:len(file.Name())-4] // remove .jmx
 
+		// loop 10 times
+		for i := 0; i < 10; i++ {
+			fmt.Println("iterasi ke-: ", i+1)
+			cmd := exec.Command(*jmeter, "-n", "-t", fileJmx, "-l", fmt.Sprintf("%s/%s%d.jtl", destDir, fileReport, i+1))
+			// show the result
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("iterasi ke-: ", i+1, " file "+fileJmx+" selesai")
+			time.Sleep(time.Second * 2)
+			fmt.Println("-------------------------------------------------------")
+			fmt.Println("")
+		}
+
+	}
 }
